dabe1d84b3e4c2f143918a7ecb355a824a24dca5
Joel S <salmer168@hotmail.com>
2019-11-19 18:20:07 -0700

    Login css

--- Back-end/index.php
+++ Back-end/index.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-</head>
-<body>
-<body>
+<link rel="stylesheet" type="text/css" href="Front-end/css/mainStyle.css"/>
+
+<div class="login">
+    <h2>Login</h2>
     <form action="home.php" method="post">
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="password" name="password" placeholder="Password" required>
+        <input type="text" name="username" placeholder="Username" required><br/>
+        <input type="password" name="password" placeholder="Password" required><br/>
         <input type="submit" value="Submit">
     </form>
-<?php
-?>
-</body>
-</html>
+</div>
